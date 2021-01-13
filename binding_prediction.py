e1a8ae348d5b576bd1d46807eb0a7bfcb693d68f
Daniel <ge24cer@mytum.de>
2021-01-13 09:10:42 +0100

    fixed a problem with a variable

--- binding_prediction.py
+++ binding_prediction.py
@@ -40,7 +40,7 @@ device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 #######################################################################################################################
 # ################################################parameters and hyperparameters#######################################
 
-number_of_random_draws = 5  # used to be 10
+number_of_random_draws = 10  # used to be 10
 batch_sizes = list(range(20, 100))
 learning_rates = [0.01, 0.001, 0.0001]
 numbers_of_epochs = list(range(100, 300))
@@ -130,8 +130,7 @@ best_parameters_overall = [0, 0, 0]
 
 current_best_r2m = 0
 
-<<<<<<< HEAD
-=======
+
 # for test_train_index in tqdm(range(3)):
 for test_train_index in tqdm(range(5)):
     for optimization in tqdm(range(number_of_random_draws)):
